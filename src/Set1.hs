dc186e165c8baebd156e251632c75f36178390d1
gfixler <gary@garyfixler.com>
2016-01-20 02:01:34 -0800

    Solve RNG/fiveRands challenge

--- src/Set1.hs
+++ src/Set1.hs
@@ -5,3 +5,12 @@ module Set1 where
 
 import MCPrelude
 
+fiveRands :: [Integer]
+fiveRands = [a,b,c,d,e]
+    where s0 = mkSeed 1
+          (a, s1) = rand s0
+          (b, s2) = rand s1
+          (c, s3) = rand s2
+          (d, s4) = rand s3
+          (e, _)  = rand s4
+
