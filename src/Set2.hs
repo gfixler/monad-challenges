ebaec49d16b9c47343c6197ac22d7732cff07b9a
gfixler <gary@garyfixler.com>
2016-01-21 02:47:55 -0800

    Solve Maybe type and its show instance

--- src/Set2.hs
+++ src/Set2.hs
@@ -5,3 +5,9 @@ module Set1 where
 
 import MCPrelude
 
+data Maybe a = Nothing | Just a
+
+instance Show a => Show (Maybe a) where
+    show Nothing = "Nothing"
+    show (Just x) = "Just " ++ show x
+
